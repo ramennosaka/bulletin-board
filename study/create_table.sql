0f2b912f67574008f02d7edccb493198ab2985db
yusuke nosaka <yusuke_nosaka@jp.honda>
2024-08-07 11:10:15 +0900

    add create_table

--- study/create_table.sql
+++ study/create_table.sql
@@ -1,21 +1,22 @@
 CREATE TABLE tb_gw_abnormal_log
 (
-    id                  BIGSERIAL PRIMARY KEY,
-    log_id              INTEGER NOT NULL,
-    abnormal_status     BOOLEAN NOT NULL,
-    occurrence_time     TIMESTAMPTZ
+    id              BIGSERIAL PRIMARY KEY,
+    log_id          INTEGER NOT NULL,
+    abnormal_status BOOLEAN NOT NULL,
+    occurrence_time TIMESTAMPTZ
 );
 
 CREATE TABLE tb_gw_log_details
 (
-    log_id  INTEGER NOT NULL PRIMARY KEY,
-    detail  VARCHAR(255)
+    log_id INTEGER NOT NULL PRIMARY KEY,
+    detail VARCHAR(255)
 );
 
-CREATE TABLE tb_gw_mc_flow_count
+CREATE TABLE tb_gw_mc_count
 (
-    id     BIGSERIAL NOT NULL PRIMARY KEY,
-    count  INTEGER NOT NULL
+    mc_count   INTEGER NOT NULL,
+    created_at TIMESTAMPTZ,
+    uuid       UUID    NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE tb_gw_weld_buffer_count
@@ -27,6 +28,14 @@ CREATE TABLE tb_gw_weld_buffer_count
 
 CREATE TABLE tb_pa_impact_info
 (
-    id              BIGSERIAL PRIMARY KEY,
-    pa_cycle_time   INTEGER NOT NULL
+    id            BIGSERIAL PRIMARY KEY,
+    pa_cycle_time INTEGER NOT NULL
 );
+
+CREATE TABLE tb_gw_remaining_model
+(
+    UUID        UUID    NOT NULL PRIMARY KEY,
+    model_count INTEGER NOT NULL,
+    created_at  TIMESTAMPTZ
+);
+
